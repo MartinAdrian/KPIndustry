1a512d8f29ac1fa34fe1ab6302b3a40e5401546a
MartinAdrian <martin.adrian96@outlook.com>
2024-12-25 15:55:35 +0200

    something went wrong

--- MyProject/KPITracker/urls.py
+++ MyProject/KPITracker/urls.py
@@ -5,4 +5,5 @@ app_name = "KPIndustry"
 
 urlpatterns = [
     path("", views.UserView.as_view(), name="userscreen"),
+    path("add-user/", views.UserView.as_view(), name="add-user"),
 ]
--- MyProject/KPITracker/views.py
+++ MyProject/KPITracker/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse
 from django.shortcuts import render
 from KPITracker.models import UserList
 
@@ -6,3 +7,11 @@ from KPITracker.models import UserList
 class UserView(ListView):
     model = UserList
     template_name = "KPITracker/userIndex.html"
+
+class CreateUserView(CreateView):
+    model = UserList
+    fields = ["FirstName", "LastName","eMail","UserType"]
+    template_name = "KPITracker/addUserForm.html"
+
+    def get_success_url(self):
+        return reverse("KPITracker/userscreen")
