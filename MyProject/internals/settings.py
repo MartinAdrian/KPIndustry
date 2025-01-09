b7985c8780bf0b8d85ee4998dfdab4f1683791ba
MartinAdrian <martin.adrian96@outlook.com>
2025-01-09 20:26:38 +0200

    Update

--- MyProject/internals/settings.py
+++ MyProject/internals/settings.py
@@ -130,4 +130,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/KPIndustry/home/"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/KPIndustry/"
